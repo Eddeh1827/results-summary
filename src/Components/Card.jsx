b2d733356c01e0b681a7a78c2521ac3fa9b9e873
Eddie Bickham <bickhame@gmail.com>
2023-09-06 01:06:28 -0700

    This is it. I think we've finally done it!

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./css/card.css";
-import memory from "./assets/images/icon-memory.svg";
-import reaction from "./assets/images/icon-reaction.svg";
-import verbal from "./assets/images/icon-verbal.svg";
-import visual from "./assets/images/icon-visual.svg";
+import "../css/card.css";
+import memory from "../assets/images/icon-memory.svg";
+import reaction from "../assets/images/icon-reaction.svg";
+import verbal from "../assets/images/icon-verbal.svg";
+import visual from "../assets/images/icon-visual.svg";
 
 function Card() {
   return (
