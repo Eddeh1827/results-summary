c6f4e4b85054a9343ec3da53bed99812c65cb8f6
Eddie Bickham <bickhame@gmail.com>
2023-09-06 01:00:19 -0700

    I think I figured it out. THanks StackOverflow 2

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Card from "../Components/Card";
-import "../css/Home.css";
+import Card from "./Components/Card";
+import "./css/Home.css";
 
 export default function Home() {
   return (
