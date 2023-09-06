a273c8aa1467f480d406431280ba345f8b684a7a
Eddie Bickham <bickhame@gmail.com>
2023-09-06 10:11:03 -0700

    data.json is semi working. Still need to work on getting the icons to display

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/card.css";
+import reaction from "../assets/images/icon-reaction.svg";
 import memory from "../assets/images/icon-memory.svg";
 import verbal from "../assets/images/icon-verbal.svg";
 import visual from "../assets/images/icon-visual.svg";
@@ -28,24 +29,24 @@ function Card(props) {
         </div>
         <div className="cards">
           <div className="card">
-            <img src={data[0].icon} alt="lightning bolt"></img>
+            <img src={reaction} alt="lightning bolt"></img>
             <span id="react">{data[0].category}</span>
             <span className="sum-bold">{data[0].score}</span> / 100
           </div>
           <div className="card">
             <img src={memory} alt="brain"></img>
-            <span id="memory">Memory</span>
-            <span className="sum-bold">92</span> / 100
+            <span id="memory">{data[1].category}</span>
+            <span className="sum-bold">{data[1].score}</span> / 100
           </div>
           <div className="card">
             <img src={verbal} alt="message box"></img>
-            <span id="verbal">Verbal</span>
-            <span className="sum-bold">61</span> / 100
+            <span id="verbal">{data[1].category}</span>
+            <span className="sum-bold">{data[2].score}</span> / 100
           </div>
           <div className="card">
             <img src={visual} alt="eye"></img>
-            <span id="visual">Visual</span>
-            <span className="sum-bold">72</span> / 100
+            <span id="visual">{data[3].category}</span>
+            <span className="sum-bold">{data[3].score}</span> / 100
           </div>
         </div>
         <button className="continue">Continue</button>
